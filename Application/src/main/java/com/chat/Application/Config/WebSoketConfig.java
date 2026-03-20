ef0c2260b8f9c9d92f7d4575bcc60c41ea9d3a93
kartiktiwari216 <kartiktiwari216@gmail.com>
2026-03-20 15:35:38 +0530

    Update WebSoketConfig.java

--- Application/src/main/java/com/chat/Application/Config/WebSoketConfig.java
+++ Application/src/main/java/com/chat/Application/Config/WebSoketConfig.java
@@ -6,21 +6,20 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSoketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*").withSockJS();
+            
+                .setAllowedOriginPatterns("*") 
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
-
     }
 }
